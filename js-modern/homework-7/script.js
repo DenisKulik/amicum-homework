8549c3f47bba43e0ccc7c0e840c55204ffbb8375
Denis Kulik <kulikdenis1996x@gmail.com>
2023-11-18 15:24:10 +0700

    js-modern: fix homework 7

--- js-modern/homework-7/script.js
+++ js-modern/homework-7/script.js
@@ -64,7 +64,25 @@ Vue.component('feedback', {
     template: `
     <footer>
       <form id="feedback-form" @submit.prevent="submitForm">
-        <!-- form contents -->
+        <form id="feedback-form" @submit.prevent="submitForm">
+        <div>
+          <label for="name">Имя:</label>
+          <input v-model="name" type="text" id="name" name="name" required />
+        </div>
+        <div>
+          <label for="phone">Телефон:</label>
+          <input v-model="phone" type="tel" id="phone" name="phone" required />
+        </div>
+        <div>
+          <label for="email">E-mail:</label>
+          <input v-model="email" type="email" id="email" name="email" required />
+        </div>
+        <div>
+          <label for="text">Текст:</label>
+          <textarea id="text" v-model="textarea" name="text"></textarea>
+        </div>
+        <button type="submit">Отправить</button>
+      </form>
       </form>
     </footer>
   `,
